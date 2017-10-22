58a84ac1ffc9629257b7f436076638b1b5a79c38
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-22 12:59:07 -0600

    working on evaluation

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -8,11 +8,12 @@ class EventReporter
 
   def initialize(filename = './data/full_event_attendees.csv')
     @filename = filename
-    @data = csv_load.map {|row| Attendee.new(row, self)}
+    @data = csv_load
   end
 
   def csv_load
-    CSV.open filename, headers: true, header_converters: :symbol
+    csv = CSV.open filename, headers: true, header_converters: :symbol
+    csv.map {|row| Attendee.new(row, self)}
   end
 
   def format_zip_codes
@@ -26,5 +27,9 @@ class EventReporter
       attendee.phone.gsub(/\D/, "").match(/^1?(\d{3})(\d{3})(\d{4})/)
     [$1, $2, $3].join("-")
     end
+  end
+
+  def find
+
   end
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -2,8 +2,11 @@ require_relative "event_reporter"
 require "pry"
 
 class Runner
+  attr_reader :reporter, :queue
 
   def initialize
+    @queue = []
+    @reporter = []
     intro
     evaluate_first_command(input_splitter)
   end
@@ -13,21 +16,46 @@ class Runner
   end
 
   def input_splitter
-    gets.chomp.split(" ")
+    STDIN.gets.chomp.split(" ")
+  end
+
+  def restart_message
+    puts "That is not an executable command.
+    Please enter a valid csv file after
+    load or we will load the full_event_attendees.csv
+    if you type load and then hit enter."
   end
 
   def evaluate_first_command(input)
     return if input[0] == 'q'
-    if input[0] == 'load' && File.exist?("./data/#{input[1]}")
-      @reporter = EventReporter.new("./data/#{input[1]}")
+    if input[0] == 'load' && File.exist?("./data/#{input[1]}") && input[1] != nil
+      @reporter << EventReporter.new("./data/#{input[1]}")
+      evaluate_next_command(input_splitter)
     elsif input[0] == 'load' && input[1].nil?
-      @reporter = EventReporter.new
+      @reporter << EventReporter.new
+      evaluate_next_command(input_splitter)
     else
-      puts "That is not an executable command.
-      Please enter a valid csv file after
-      load or we will load the full_event_attendees.csv
-      if you type load then enter"
+      restart_message
+      evaluate_first_command(input_splitter)
     end
+  end
+
+  def evaluate_next_command(input)
+    if input[0] == 'queue'
+      queue(input)
+    elsif input[0] == 'find'
+      find(input)
+    else
+      evaluate_next_command(input_splitter)
+    end
+  end
+
+  def find(input)
+    all = @reporter.data.find_all do |attendee|
+      binding.pry
+      attendee.input[1] == input[2]
+    end
+    @queue << all
   end
 end
 

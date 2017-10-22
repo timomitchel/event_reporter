923dc07c0077ce43d9df8c6c2d3a2ab9a17c21f9
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-22 16:31:40 -0600

    formatted attendees

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -3,19 +3,26 @@ require "pry"
 class Attendee
 
   attr_reader :id, :date, :first_name, :last_name,
-              :email, :phone, :street, :city, :state,
-              :zipcode
-  def initialize(info, parent)
+              :email, :phone, :street, :city, :state, :zipcode
+  def initialize(info)
     @id = info[:'']
     @date = Time.strptime(info[:regdate], '%D %R')
     @first_name = info[:first_name]
     @last_name = info[:last_name]
     @email = info[:email_address]
-    @phone = info[:homephone]
+    @phone = clean_phone_numbers(info[:homephone])
     @street = info[:street]
     @city = info[:city]
     @state = info[:state]
-    @zipcode = info[:zipcode]
-    @parent = parent
+    @zipcode = clean_zipcode(info[:zipcode])
+  end
+
+  def clean_zipcode(zipcode_entry)
+    zipcode_entry.to_s.rjust(5, '0')[0..4]
+  end
+
+  def clean_phone_numbers(number)
+    number.gsub(/\D/, "").match(/^1?(\d{3})(\d{3})(\d{4})/)
+  [$1, $2, $3].join("-")
   end
 end
--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -4,32 +4,18 @@ require "pry"
 
 class EventReporter
 
-  attr_reader :filename, :data, :formatted_data
+  attr_reader :filename, :data
 
   def initialize(filename = './data/full_event_attendees.csv')
     @filename = filename
     @data = csv_load
+    binding.pry
   end
 
   def csv_load
     csv = CSV.open filename, headers: true, header_converters: :symbol
-    csv.map {|row| Attendee.new(row, self)}
-  end
-
-  def format_zip_codes
-    data.map do |attendee|
-      attendee.zipcode.to_s.rjust(5, '0')[0..4]
-    end
-  end
-
-  def format_phone_numbers
-    data.map do |attendee|
-      attendee.phone.gsub(/\D/, "").match(/^1?(\d{3})(\d{3})(\d{4})/)
-    [$1, $2, $3].join("-")
-    end
-  end
-
-  def find
-
+    csv.map {|row| Attendee.new(row)}
   end
 end
+
+EventReporter.new

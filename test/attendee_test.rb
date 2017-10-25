071bcd6b910ff2418ab525d68456dc9090b107fe
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-25 10:21:00 -0600

    attendee tests

--- test/attendee_test.rb
+++ test/attendee_test.rb
@@ -6,11 +6,33 @@ require "./lib/attendee"
 
 class AttendeeTest < Minitest::Test
 
-  def test_attributes_return_correctly
-    reporter = EventReporter.new
-    attendee = Attendee.new(reporter.data)
+  attr_reader :attendee, :reporter
 
-    assert_equal "", attendee.id
+  def setup
+    @reporter = EventReporter.new
+    @attendee = Attendee.new({'': 1,
+                              regdate: '11/12/08 10:47',
+                              first_name: "Allison",
+                              last_name: "Nguyen",
+                              email_address: "arannon@jumpstartlab.com",
+                              homephone: '6154385000',
+                              street: "3155 19th St NW",
+                              city: "Washington",
+                              state: "dc",
+                              zipcode: "20010"})
+  end
+
+  def test_attributes_return_correctly
+    assert_equal 1, attendee.id
+    assert_equal '2008-11-12 10:47:00 -0700', attendee.date
+    assert_equal 'allison', attendee.first_name
+    assert_equal 'nguyen', attendee.last_name
+    assert_equal 'arannon@jumpstartlab.com', attendee.email
+    assert_equal '615-438-5000', attendee.phone
+    assert_equal '3155 19th st nw', attendee.street
+    assert_equal 'washington', attendee.city
+    assert_equal 'dc', attendee.state
+    assert_equal '20010', attendee.zipcode
   end
 
 end
